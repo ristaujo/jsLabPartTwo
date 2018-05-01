46d719279322d44bc04e5cbf2c40967295bd8fe5
John <ristaujo@gmail.com>
2018-05-01 18:39:51 -0400

    Added prompt before each loop

--- script.js
+++ script.js
@@ -63,7 +63,6 @@ if (choice === "yes") {
 } */
 
 let character = null;
-let play = false;
 
 function getDamage() {
   let damage = Math.floor((Math.random() * 5) + 1);
@@ -73,40 +72,46 @@ function getDamage() {
 function startCombat() {
   let play = prompt("Would you like to attack or quit?");
 
-  //while (play === "attack") {
+    if (play === "attack") {
 
+      let userHealth = 40;
+      let grantHealth = 10;
+      
+      let userWins = 0;
+      let grantWins = 0;
+      
+      while (userHealth > 0 && grantHealth > 0) {
 
-    let userHealth = 40;
-    let grantHealth = 10;
-    
-    let userWins = 0;
-    let grantWins = 0;
-    
-    while (userHealth > 0 && grantHealth > 0) {
+        userHealth -= getDamage();
+        grantHealth -= getDamage();
+      
+        console.log(` ${character}'s health is ${userHealth}`)
+        console.log(` Grant's health is ${grantHealth}`)
 
-      userHealth -= getDamage();
-      grantHealth -= getDamage();
-    
-      console.log(` ${character}'s health is ${userHealth}`)
-      console.log(` Grant's health is ${grantHealth}`)
+        let again = prompt("Would you like to attack or quit?");
 
-
-    
-    
-    
-      if (grantHealth <= 0) {
-        userWins ++;
-        grantHealth = 10;
-        console.log("Grant is defeated and user gets win number " + userWins)
-      } if (userWins === 3) {
-        console.log(`${character} is the winner!`);
-        break;
-      }
-      if (userHealth <= 0) {
-        console.log("Grant is the winner");
-        break; 
-      }
+      
+      
+      
+        if (grantHealth <= 0) {
+          userWins ++;
+          grantHealth = 10;
+          console.log("Grant is defeated and user gets win number " + userWins)
+        } if (userWins === 3) {
+          console.log(`${character} is the winner!`);
+          break;
+        }
+        if (userHealth <= 0) {
+          console.log("Grant is the winner");
+          break; 
+        }
+      } 
+    } else {
+      console.log('goodbye');
     }
+    if (play === "quit" || again === "quit") {
+      return 'goodbye';
+    } 
   } 
 //} 
 
